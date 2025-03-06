c2b67b2aee852628153343e7e35702a438ec5826
Elizabeth Nathania W <liza_1805@hotmail.com>
2025-03-06 16:10:12 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@
 </head>
 <body>
     <h2>Tambah Data</h2>
-    <form action="../src/insert.php" method="POST">
+    <form action="insert.php" method="POST">
         <input type="text" name="name" placeholder="Nama" required>
         <input type="email" name="email" placeholder="Email" required>
         <button type="submit">Tambah</button>
     </form>
     <h2>Lihat Data</h2>
-    <a href="../src/view_data.php">Lihat Semua Data</a>
+    <a href="view_data.php">Lihat Semua Data</a>
 </body>
 </html>
